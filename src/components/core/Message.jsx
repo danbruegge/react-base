8449339f368fc2f7ea98eb1e164a294b90c9ac0d
Daniel Brüggemann <github@barfooz.com>
2017-09-12 22:30:00 +0200

    [New] Add styled-components with basic styling

--- src/components/core/Message.jsx
+++ src/components/core/Message.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Message = ({ children }) => <p>{children}</p>;
+const Item = styled.p`
+    color: DimGrey;
+    font-size: 25px;
+`;
+
+const Message = ({ children }) => <Item>{children}</Item>;
 
 export default Message;
--- src/components/core/Title.jsx
+++ src/components/core/Title.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const Title = ({ children }) => <h1>{children}</h1>;
+const StyledTitle = styled.h1`
+    color: SeaGreen;
+    font-size: 40px;
+`;
+
+const Title = ({ children }) => <StyledTitle>{children}</StyledTitle>;
 
 export default Title;

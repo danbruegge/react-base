f92e05f1328c1d75c9a7bd97fe481ce2e744dfd2
Daniel Brüggemann <github@barfooz.com>
2018-02-15 10:24:06 +0100

    [New] Add stylelint

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import webpack from 'webpack';
+import StyleLintPlugin from 'stylelint-webpack-plugin';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
@@ -55,6 +56,9 @@ export default (env) => {
                 IS_PRODUCTION: JSON.stringify(isProduction),
             }),
             new webpack.optimize.ModuleConcatenationPlugin(),
+            new StyleLintPlugin({
+                files: ['**/*.js*'],
+            }),
             htmlSinglePage,
         ],
         devServer: {

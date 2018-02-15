469a75b29df9e3d8ae0c6dc33404f628bf8e073b
Daniel Brüggemann <github@barfooz.com>
2018-02-15 09:01:13 +0100

    [New] Use jest-styled-components

--- src/Home/index.test.jsx
+++ src/Home/index.test.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import 'jest-styled-components';
 
 import Home from 'Home';
 
-describe('Home', () => {
-    test('should render component', () => {
-        const component = <Home />;
-        const tree = renderer.create(component).toJSON();
+test('Render Home correctly', () => {
+    const component = <Home />;
+    const tree = renderer.create(component).toJSON();
 
-        expect(tree).toMatchSnapshot();
-    });
+    expect(tree).toMatchSnapshot();
 });
+
